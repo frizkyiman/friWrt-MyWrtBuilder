251fa2486596c1b2dccfcc57748e9e3d2b1d1420
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-11-12 20:41:55 +0700

    Update builder-patch-immortalwrt.sh

--- scripts/builder-patch-immortalwrt.sh
+++ scripts/builder-patch-immortalwrt.sh
@@ -2,6 +2,8 @@
 
 echo "Start Builder Patch !"
 echo "Current Path: $PWD"
+source_variable=$BRANCH
+branch_main=$(echo $source_variable | awk -F'.' '{print $1"."$2}')
 
 cd $GITHUB_WORKSPACE/$BASE-imagebuilder-$BRANCH-bcm27xx-bcm2711.Linux-x86_64 || exit
 
@@ -10,8 +12,8 @@ sed -i "/luci-app-cpufreq/d" include/target.mk
 
 # Custom Repository
 #sed -i '12i\src/gz IceG_repo https://github.com/4IceG/Modem-extras/raw/main/myrepo' repositories.conf
-sed -i '12i\src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/generic' repositories.conf
-sed -i '13i\src/gz custom_arch https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/aarch64_cortex-a72' repositories.conf
+sed -i '12i\src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/$branch_main/generic' repositories.conf
+sed -i '13i\src/gz custom_arch https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/$branch_main/aarch64_cortex-a72' repositories.conf
 sed -i 's/option check_signature/# option check_signature/g' repositories.conf
 
 # Force opkg to overwrite files
