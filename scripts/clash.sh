4403761078d4752aedcebc797c348c5d064c1823
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-11-12 16:10:43 +0700

    Update clash.sh

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -9,7 +9,7 @@ file="clash.meta-linux-arm64-v1.16.0.gz"
 mkdir -p files/etc/openclash/core
 cd files/etc/openclash/core || (echo "Clash core path does not exist! " && exit)
 wget -q $download_url
-{ tar -zxvf "$file" && echo "Success!"; } ||
-{ gzip -d "$file" && echo "Success!"; } ||
+{ tar -zxvf "$file" && echo "Success using tar!"; } ||
+{ gzip -d "$file" && echo "Success using gzip!"; } ||
 echo "Failed to extract file!";
 rm -rf "$file"
