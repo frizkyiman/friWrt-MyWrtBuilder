21224c1e1f7cd68ceefd60af1d462cb9c1d587a7
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-11-13 22:50:42 +0700

    @

--- scripts/clash-core.sh
+++ scripts/clash-core.sh
@@ -3,17 +3,24 @@
 echo "Start Clash Core Download !"
 echo "Current Path: $PWD"
 
-meta_core="https://github.com/MetaCubeX/Clash.Meta/releases/download/v1.16.0/clash.meta-linux-arm64-v1.16.0.gz"
-clash_core="https://github.com/vernesong/OpenClash/blob/core/master/dev/clash-linux-arm64.tar.gz"
-clash_tun_core="https://github.com/vernesong/OpenClash/blob/core/master/premium/clash-linux-arm64-2023.08.17.gz"
-file="*-arm64-*.gz"
-
 mkdir -p files/etc/openclash/core
 cd files/etc/openclash/core || { echo "Clash core path does not exist!"; exit 1; }
-wget "$meta_core" "$clash_core" "$clash_tun_core"
 
-{ tar -zxvf "$file" && echo "Success using tar!"; } ||
-{ gzip -d "$file" && echo "Success using gzip!"; } ||
-echo "Failed to extract file!"
+urls=(
+  "https://github.com/MetaCubeX/Clash.Meta/releases/download/v1.16.0/clash.meta-linux-arm64-v1.16.0.gz"
+  "https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-arm64.tar.gz"
+  "https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-arm64-2023.08.17.gz"
+)
+targets=("clash_meta" "clash" "clash_tun")
 
-rm -rf "$file"
+extract_and_cleanup() {
+  { tar -zxvf "$1" && rm -f "$1" && echo "Success using tar!"; } ||
+  { gzip -d -c "$1" > "$2" && rm -f "$1" && echo "Success using gzip!"; } ||
+  echo "Failed to extract $1"
+}
+
+for ((i=0; i<${#urls[@]}; i++)); do
+  wget "${urls[i]}" && extract_and_cleanup "$(basename "${urls[i]}")" "${targets[i]}"
+done
+
+
