be7abfaf67889f17ebc7e17fdf7d87b386df8383
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-11-18 20:36:14 +0700

    fix config editor not shown

--- files-openwrt/usr/bin/patchoc.sh
+++ files-openwrt/usr/bin/patchoc.sh
@@ -61,7 +61,7 @@ then
 EOF
 fi
 
-if ! grep -q "Config Editor" $CONT && [ -f "/www/tinyfm/tinyfm.php" ]; then
+if ! grep -q "Config Editor" $CONT && [ -f "/www/tinyfm/index.php" ]; then
     sed -i '87 i\	entry({"admin", "services", "openclash", "editor"}, template("openclash/editor"),_("Config Editor"), 90).leaf = true' $CONT
     cat << EOF > /usr/lib/lua/luci/view/openclash/editor.htm
 <%+header%>
@@ -69,11 +69,11 @@ if ! grep -q "Config Editor" $CONT && [ -f "/www/tinyfm/tinyfm.php" ]; then
 <iframe id="editor" style="width: 100%; min-height: 100vh; border: none; border-radius: 2px;"></iframe>
 </div>
 <script type="text/javascript">
-document.getElementById("editor").src = "http://" + window.location.hostname + "/tinyfm/tinyfm.php?p=etc/openclash";
+document.getElementById("editor").src = "http://" + window.location.hostname + "/tinyfm/index.php?p=etc/openclash";
 </script>
 <%+footer%>
 EOF
-elif grep -q "Config Editor" $CONT && [ ! -f "/www/tinyfm/tinyfm.php" ]; then
+elif grep -q "Config Editor" $CONT && [ ! -f "/www/tinyfm/index.php" ]; then
 	sed -i '/Config Editor/d' $CONT
 fi
 
