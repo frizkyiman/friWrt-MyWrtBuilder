707bd6fdbf6885535409457fc88a3d37305ea885
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-11-20 14:40:53 +0700

    Update builder-patch-immortalwrt.sh

--- scripts/builder-patch-immortalwrt.sh
+++ scripts/builder-patch-immortalwrt.sh
@@ -7,15 +7,12 @@ cd $GITHUB_WORKSPACE/$WORKING_DIR || exit
 
 version=$(echo "$BRANCH" | cut -d'.' -f1)
 branch_main=$( [ "$version" == "21" ] && echo "$BRANCH" | awk -F'.' '{print $1"."$2}' || echo "main" )
+option_squashfs=$( [ $ROOTFS_SQUASHFS" == "true" ] && echo "CONFIG_TARGET_ROOTFS_SQUASHFS=y" || echo "# CONFIG_TARGET_ROOTFS_SQUASHFS is not set" )
 
-if [ "$ROOTFS_SQUASHFS" == "true" ]; then
-    option_squashfs="CONFIG_TARGET_ROOTFS_SQUASHFS=y"
-else
-    option_squashfs="# CONFIG_TARGET_ROOTFS_SQUASHFS is not set"
-fi
-
-# Remove redundant default packages
-sed -i "/luci-app-cpufreq/d" include/target.mk
+# custom repo and Disable opkg signature check
+sed -i "43i\sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf" /files-openwrt/etc/uci-defaults/99-init-settings.sh
+sed -i "44i\echo "src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/$branch_main/generic" >> /etc/opkg/customfeeds.conf" /files/etc/uci-defaults/99-init-settings.sh
+sed -i "45i\echo "src/gz custom_arch https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/$branch_main/$(cat /etc/os-release | grep OPENWRT_ARCH | awk -F '"' '{print $2}')" >> /etc/opkg/customfeeds.conf" /files/etc/uci-defaults/99-init-settings.sh
 
 # Custom Repository
 sed -i "13i\src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/$branch_main/generic" repositories.conf
