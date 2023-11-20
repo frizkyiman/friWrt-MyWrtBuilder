a3ecd2c8b4a57b9ed568a14d8cc4321800dff8de
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-11-20 21:06:48 +0700

    Update build-immortalwrt.sh

--- build-immortalwrt.sh
+++ build-immortalwrt.sh
@@ -59,7 +59,7 @@ PACKAGES="$PACKAGES sudo adb parted losetup resize2fs luci luci-ssl block-mount 
 
 # some custom .ipk files
 FILES="files"
-CUSTOM=$( [ "$TARGET" == "rpi-4" ] && echo "kmod-i2c-bcm2835 kmod-rtl8821cu" )
+CUSTOM=$( [ "$TARGET" == "rpi-4" ] && echo "kmod-i2c-bcm2835" )
 
 # Exclude package
 EXCLUDED="-dnsmasq -automount -libustream-openssl"
