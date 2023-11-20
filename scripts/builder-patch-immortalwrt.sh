a7ab75d8aae4e113aadd85b37ba411bc50a9f162
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-11-20 21:29:52 +0700

    Update builder-patch-immortalwrt.sh

--- scripts/builder-patch-immortalwrt.sh
+++ scripts/builder-patch-immortalwrt.sh
@@ -11,8 +11,8 @@ option_squashfs=$( [ "$ROOTFS_SQUASHFS" == "true" ] && echo "CONFIG_TARGET_ROOTF
 
 # custom repo and Disable opkg signature check
 sed -i "43i\sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf" /files/etc/uci-defaults/99-init-settings.sh
-sed -i "44i\echo "src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/$branch_main/generic" >> /etc/opkg/customfeeds.conf" /files/etc/uci-defaults/99-init-settings.sh
-sed -i "45i\echo "src/gz custom_arch https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/$branch_main/$(cat /etc/os-release | grep OPENWRT_ARCH | awk -F '"' '{print $2}')" >> /etc/opkg/customfeeds.conf" /files/etc/uci-defaults/99-init-settings.sh
+sed -i "44i\echo 'src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/$branch_main/generic' >> /etc/opkg/customfeeds.conf" /files/etc/uci-defaults/99-init-settings.sh
+sed -i "45i\echo 'src/gz custom_arch https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/$branch_main/$(cat /etc/os-release | grep OPENWRT_ARCH | awk -F '\"' '{print $2}')' >> /etc/opkg/customfeeds.conf" /files/etc/uci-defaults/99-init-settings.sh
 
 # Custom Repository
 sed -i "13i\src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/$branch_main/generic" repositories.conf
