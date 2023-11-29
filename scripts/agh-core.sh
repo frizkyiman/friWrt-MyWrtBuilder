0f52412f7173661d3ffe1df275ea04b1da95cac0
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-11-29 20:50:31 +0700

    @

---
+++ scripts/agh-core.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Start AdGuardHome Core Download !"
+echo "Current Path: $PWD"
+
+branch_tag=$( [ "$BRANCH" == "21.02.7" ] && echo -"$BRANCH" | awk -F'.' '{print $1"."$2}' )
+if [ ! -d "files/"$BASE""$branch_tag"/opt/AdGuardHome" ]; then
+        echo "AdGuardHome core path does not exist!
+        mkdir -p files/"$BASE""$branch_tag"/opt/AdGuardHome
+fi
+
+cd files/"$BASE""$branch_tag"/opt/AdGuardHome || { echo "AdGuardHome core path does not exist!"; exit 1; }
+
+wget https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.41/AdGuardHome_linux_arm64.tar.gz
+tar -zxf AdGuardHome_linux_arm64.tar.gz
