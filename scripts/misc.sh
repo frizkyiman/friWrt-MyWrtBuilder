bfecf772019303ef722cf21f7154ee6e6925368a
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-01 23:56:53 +0700

    Update misc.sh

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -3,7 +3,7 @@
 echo "Start Downloading Misc files !"
 echo "Current Path: $PWD"
 
-branch_main=$( [ "$BRANCH" == "21.02.7" ] && echo -"$BRANCH" | awk -F'.' '{print $1"."$2}' )
+# custom script files urls
 ARCHH=$( [ "$TARGET" == "rpi-4" ] && echo "aarch64" || echo "x86_64" )
 urls=("https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-$ARCHH.tgz"
       "https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch"
@@ -12,7 +12,7 @@ urls=("https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-$ARCHH.tgz"
       "https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/usr/bin/repair_ro"
       "https://raw.githubusercontent.com/frizkyiman/auto-mount-hdd/main/mount_hdd")
 
-
+# dir files
 mkdir -p files/etc/uci-defaults
 mkdir -p files/etc/init.d
 mkdir -p files/usr/bin/
@@ -20,23 +20,25 @@ mkdir -p files/sbin/
 mkdir -p files/www/luci-static/resources/view/status/include
 cd files/
 
-wget --no-check-certificate -nv https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro
-mv files//repair_ro files/etc/init.d/repair_ro
 wget --no-check-certificate -nv "$urls"
 
-tar -xzvf files/usr/bin/*-speedtest-*.tgz -C files/usr/bin
-rm files/*-speedtest-*.tgz
-rm files/usr/bin/speedtest.md
+# files/etc/uci-defaults
+mv files/99-init-settings_"$BASE""$branch_main".sh files/etc/uci-defaults/99-init-settings.sh
 
+# files/etc/init.d
+wget --no-check-certificate -nv https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro -O files/etc/init.d/repair_ro
+
+# files/sbin/
 echo "sync && echo 3 > /proc/sys/vm/drop_caches && rm -rf /tmp/luci* >> files/sbin/free.sh
 mv files/sync_time.sh files/sbin/sync_time.sh
+
+# files/usr/bin/
 mv files/neofetch files/usr/bin/neofetch
 mv files/clock files/usr/bin/clock
 mv files/repair_ro files/usr/bin/repair_ro
 mv files/mount_hdd files/usr/bin/mount_hdd
-mv files/patchoc.sh files/usr/bin/patchoc.sh 
+mv files/patchoc.sh files/usr/bin/patchoc.sh
+tar -xzvf files/usr/bin/*-speedtest-*.tgz -C files/usr/bin && rm files/*-speedtest-*.tgz && rm files/usr/bin/speedtest.md
 
+# files/www/luci-static/resources/view/status/include
 mv files/10_system_$BASE.js files/www/luci-static/resources/view/status/include/10_system.js
-mv files/99-init-settings_"$BASE""$branch_main".sh files/etc/uci-defaults/99-init-settings.sh
-
-
