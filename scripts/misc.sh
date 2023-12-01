e1f732ef380d603115712882154485d9775c1fbe
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-02 00:32:38 +0700

    Update misc.sh

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -19,27 +19,33 @@ mkdir -p files/usr/bin/
 mkdir -p files/sbin/
 mkdir -p files/www/luci-static/resources/view/status/include
 
-# download files
-wget --no-check-certificate -nv -P files "${urls[@]}"
-cd files/
+# Download files
+if wget --no-check-certificate -nv -P files "${urls[@]}"; then
+    # files/etc/uci-defaults
+    mv files/99-init-settings_"$BASE""$branch_main".sh files/etc/uci-defaults/99-init-settings.sh
 
-# files/etc/uci-defaults
-mv files/99-init-settings_"$BASE""$branch_main".sh files/etc/uci-defaults/99-init-settings.sh
+    # files/etc/init.d
+    if wget --no-check-certificate -nv https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro -O files/etc/init.d/repair_ro; then
+        # files/sbin/
+        echo "sync && echo 3 > /proc/sys/vm/drop_caches && rm -rf /tmp/luci*" >> files/sbin/free.sh
+        mv files/sync_time.sh files/sbin/sync_time.sh
 
-# files/etc/init.d
-wget --no-check-certificate -nv https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro -O files/etc/init.d/repair_ro
+        # files/usr/bin/
+        mv files/neofetch files/usr/bin/neofetch
+        mv files/clock files/usr/bin/clock
+        mv files/repair_ro files/usr/bin/repair_ro
+        mv files/mount_hdd files/usr/bin/mount_hdd
+        mv files/patchoc.sh files/usr/bin/patchoc.sh
+        tar -xzvf files/ookla-speedtest-1.2.0-linux-$ARCHH.tgz -C files/usr/bin && rm files/ookla-speedtest-1.2.0-linux-$ARCHH.tgz && rm files/usr/bin/speedtest.md
 
-# files/sbin/
-echo "sync && echo 3 > /proc/sys/vm/drop_caches && rm -rf /tmp/luci*" >> files/sbin/free.sh
-mv files/sync_time.sh files/sbin/sync_time.sh
+        # files/www/luci-static/resources/view/status/include
+        mv files/10_system_$BASE.js files/www/luci-static/resources/view/status/include/10_system.js
+    else
+        echo "Error downloading repair_ro script."
+    fi
+else
+    echo "Error downloading files. Exiting."
+    exit 1
+fi
 
-# files/usr/bin/
-mv files/neofetch files/usr/bin/neofetch
-mv files/clock files/usr/bin/clock
-mv files/repair_ro files/usr/bin/repair_ro
-mv files/mount_hdd files/usr/bin/mount_hdd
-mv files/patchoc.sh files/usr/bin/patchoc.sh
-tar -xzvf files/*-speedtest-*.tgz -C files/usr/bin && rm files/*-speedtest-*.tgz && rm files/usr/bin/speedtest.md
-
-# files/www/luci-static/resources/view/status/include
-mv files/10_system_$BASE.js files/www/luci-static/resources/view/status/include/10_system.js
+echo "Download and configuration completed!"
