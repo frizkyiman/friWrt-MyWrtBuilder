e0234126a619032095251b7c13633912fb6b68e5
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-01 21:01:00 +0700

    Update agh-core.sh

--- scripts/agh-core.sh
+++ scripts/agh-core.sh
@@ -5,12 +5,11 @@ echo "Current Path: $PWD"
 
 branch_tag=$( [ "$BRANCH" == "21.02.7" ] && echo -"$BRANCH" | awk -F'.' '{print $1"."$2}' )
 if [ ! -d "files/"$BASE""$branch_tag"/opt/AdGuardHome" ]; then
-        echo "AdGuardHome core path does not exist!
+        echo "AdGuardHome core path does not exist!"
         mkdir -p files/"$BASE""$branch_tag"/opt/AdGuardHome
 fi
 
 cd files/"$BASE""$branch_tag"/opt/AdGuardHome || { echo "AdGuardHome core path does not exist!"; exit 1; }
-
 wget -q https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.41/AdGuardHome_linux_arm64.tar.gz
 tar -zxf AdGuardHome_linux_arm64.tar.gz
 
