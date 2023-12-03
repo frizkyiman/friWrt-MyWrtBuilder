f6d99e8f097e451f4ac0c581177c080ec44c17ee
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-03 17:58:50 +0700

    Update agh-core.sh

--- scripts/agh-core.sh
+++ scripts/agh-core.sh
@@ -7,5 +7,4 @@ ARCHHH=$( [ "$TARGET" == "rpi-4" ] && echo "arm64" || echo "amd64" )
 
 cd files/opt/AdGuardHome || { echo "AdGuardHome core path does not exist!"; exit 1; }
 wget -nv https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.41/AdGuardHome_linux_$ARCHHH.tar.gz
-tar -zxf AdGuardHome_linux_$ARCHHH.tar.gz
-rm files/opt/AdGuardHome/*.gz
+tar -zxf AdGuardHome_linux_$ARCHHH.tar.gz && rm files/opt/AdGuardHome/AdGuardHome_linux_$ARCHHH.tar.gz
