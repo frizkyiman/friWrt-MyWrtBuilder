9944009ec472086f1eb2edde50cd0449339def85
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-03 17:46:11 +0700

    fix typo

--- scripts/clash-core.sh
+++ scripts/clash-core.sh
@@ -5,7 +5,7 @@ echo "Current Path: $PWD"
 echo "Start YACD Download !"
 mkdir -p files/usr/share/openclash/ui
 wget --no-check-certificate -nv https://github.com/taamarin/yacd-meta/archive/gh-pages.zip -O files/usr/share/openclash/ui/yacd.zip
-unzip files/usr/share/openclash/ui/yacd.zip -d files/usr/share/openclash/ui
+unzip -qq files/usr/share/openclash/ui/yacd.zip -d files/usr/share/openclash/ui
 mv "files/usr/share/openclash/ui/yacd-*" "files/usr/share/openclash/ui/yacd.new"
 rm files/usr/share/openclash/ui/yacd.zip
 
