9dc5517e7a3535ec20d9c02001ac039d4c8c3058
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-05 12:38:32 +0700

    fix and update logic structure

--- scripts/clash-core.sh
+++ scripts/clash-core.sh
@@ -3,31 +3,41 @@
 echo "Current Path: $PWD"
 
 echo "Start YACD Download !"
+#custom yacd download url
+yacd="https://github.com/taamarin/yacd-meta/archive/gh-pages.zip"
+
 if [ -d files/usr/share/openclash/ui ]; then
    mkdir -p files/usr/share/openclash/ui
-   if wget --no-check-certificate -nv https://github.com/taamarin/yacd-meta/archive/gh-pages.zip -O files/usr/share/openclash/ui/yacd.zip; then
-      unzip -qq files/usr/share/openclash/ui/yacd.zip -d files/usr/share/openclash/ui
+   if wget --no-check-certificate -q $yacd -O files/usr/share/openclash/ui/yacd.zip; then
+      unzip -qq files/usr/share/openclash/ui/yacd.zip -d files/usr/share/openclash/ui && rm files/usr/share/openclash/ui/yacd.zip
       mv files/usr/share/openclash/ui/yacd-* files/usr/share/openclash/ui/yacd.new
-      rm files/usr/share/openclash/ui/yacd.zip
+      echo "YACD Dashboard download successfully."
+   else
+      echo "Failed to download YACD Dashboard"
    fi
 fi
 
-
 echo "Start Clash Core Download !"
+arch=$( [ "$TARGET" == "rpi-4" ] && echo "arm64" || echo "amd64" )
+#core download url
+clash_meta="https://github.com/MetaCubeX/mihomo/releases/download/v1.16.0/clash.meta-linux-$arch-v1.16.0.gz"
+clash_tun="https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-$arch-2023.08.17-13-gdcc8d87.gz"
+clash="https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-$arch.tar.gz"
+
 if [ -d files/etc/openclash/core ]; then
    mkdir -p files/etc/openclash/core
    cd files/etc/openclash/core || { echo "Clash core path does not exist!"; exit 1; }
 
    echo "Downloading clash_meta.gz..."
-   if wget -nv -O clash_meta.gz https://github.com/MetaCubeX/Clash.Meta/releases/download/v1.16.0/clash.meta-linux-arm64-v1.16.0.gz; then
+   if wget --no-check-certificate -q -O clash_meta.gz $clash_meta; then
       gzip -d clash_meta.gz
       echo "clash_meta.gz downloaded successfully."
    else
       echo "Failed to download clash_meta.gz."
    fi
-
+   
    echo "Downloading clash_tun.gz..."
-   if wget -nv -O clash_tun.gz https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-arm64-2023.08.17-13-gdcc8d87.gz; then
+   if wget --no-check-certificate -q -O clash_tun.gz $clash_tun; then
       gzip -d clash_tun.gz
       echo "clash_tun.gz downloaded successfully."
    else
@@ -35,7 +45,7 @@ if [ -d files/etc/openclash/core ]; then
    fi
 
    echo "Downloading clash.tar.gz..."
-   if wget -nv -O clash.tar.gz https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-arm64.tar.gz; then
+   if wget --no-check-certificate-q -O clash.tar.gz $clash; then
       tar -zxf clash.tar.gz
       rm clash.tar.gz
       echo "clash.tar.gz downloaded and extracted successfully."
