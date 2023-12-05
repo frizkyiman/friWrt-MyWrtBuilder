f9e48499dd217bc52adae858ada5b5832d889912
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-05 18:38:25 +0700

    Update misc.sh

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -14,7 +14,7 @@ urls=("https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-$ARCHH.
       "https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch"
       "https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/sbin/sync_time.sh"
       "https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/usr/bin/clock"
-      "https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/usr/bin/repair_ro"
+      "https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/sbin/repair_ro"
       "https://raw.githubusercontent.com/frizkyiman/auto-mount-hdd/main/mount_hdd")
 
 echo "setup 99-init-settings.sh"
@@ -33,18 +33,30 @@ echo "Downloading custom script"
 mkdir -p files/etc/init.d
 mkdir -p files/sbin/
 if wget --no-check-certificate -nv -P files "${urls[@]}"; then
-    wget --no-check-certificate -nv https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/etc/init.d/repair_ro -O files/etc/init.d/repair_ro
     echo "sync && echo 3 > /proc/sys/vm/drop_caches && rm -rf /tmp/luci*" >> files/sbin/free.sh
     mv files/sync_time.sh files/sbin/sync_time.sh
     mv files/neofetch files/usr/bin/neofetch
     mv files/clock files/usr/bin/clock
-    mv files/repair_ro files/usr/bin/repair_ro
+    mv files/repair_ro files/sbin/repair_ro
     mv files/mount_hdd files/usr/bin/mount_hdd
     tar -zxf files/ookla-speedtest-1.2.0-linux-$ARCHH.tgz -C files/usr/bin && rm files/ookla-speedtest-1.2.0-linux-$ARCHH.tgz && rm files/usr/bin/speedtest.md
-else
-    echo "Error downloading files. Exiting."
-    exit 1
 fi
+
+cat <<'EOF' >/files/etc/init.d/repair_ro
+#!/bin/sh /etc/rc.common
+
+START=99
+
+start() {
+    /sbin/repair_ro
+}
+EOF
+
+cat <<'EOF' >/files/usr/bin/repair_ro
+#!/bin/sh
+root_device="$1"
+/sbin/repair_ro "$root_device"
+EOF
 
 cp /packages/luci-app-oled_1.0_all.ipk /files/root/luci-app-oled_1.0_all.ipk
 sed -i '/reboot/ i\opkg install /root/luci-app-oled_1.0_all.ipk --force-reinstall' files/etc/uci-defaults/99-init-settings.sh
@@ -57,13 +69,12 @@ sed -i '/reboot/ i\chmod +x /usr/bin/neofetch' files/etc/uci-defaults/99-init-se
 sed -i '/reboot/ i\chmod +x /usr/bin/clock' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\chmod +x /etc/init.d/repair_ro' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\chmod +x /usr/bin/repair_ro' files/etc/uci-defaults/99-init-settings.sh
+sed -i '/reboot/ i\chmod +x /sbin/repair_ro' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\chmod +x /usr/bin/mount_hdd' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\chmod +x /usr/bin/speedtest' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\chmod +x /usr/bin/enable-agh' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\chmod +x /usr/bin/disable-agh' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\bash /usr/bin/enable-agh' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\uci set luci.main.mediaurlbase='/luci-static/bootstrap' && uci commit' files/etc/uci-defaults/99-init-settings.sh
-
-sed -i '/reboot/ i\exec >/root/first-boot.log 2>&1' files/etc/uci-defaults/99-init-settings.sh
 
 echo "Download and configuration completed!"
