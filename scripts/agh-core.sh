38064531c737865a00eaf11b2cf180e98e05cebc
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-05 19:02:25 +0700

    adding logic to use latest version of core

--- scripts/agh-core.sh
+++ scripts/agh-core.sh
@@ -4,9 +4,14 @@ echo "Start AdGuardHome Core Download !"
 echo "Current Path: $PWD"
 
 ARCHHH=$( [ "$TARGET" == "rpi-4" ] && echo "arm64" || echo "amd64" )
+repo_url="https://github.com/AdguardTeam/AdGuardHome/releases"
+latest_version=$(curl -sSL "$repo_url/latest" | grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' | head -n 1)
+url="$repo_url/download/$latest_version/AdGuardHome_linux_$ARCHHH.tar.gz"
 
 cd files/opt || { echo "AdGuardHome core path does not exist!"; exit 1; }
-if wget -nv https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.41/AdGuardHome_linux_$ARCHHH.tar.gz; then
+
+if wget -nv "$url"; then
+  echo "AdGuardHome $latest_version"
   echo "extracting core"
-  tar -zxf AdGuardHome_linux_$ARCHHH.tar.gz && rm AdGuardHome_linux_$ARCHHH.tar.gz
+  tar -zxf "AdGuardHome_linux_$ARCHHH.tar.gz" && rm "AdGuardHome_linux_$ARCHHH.tar.gz"
 fi
