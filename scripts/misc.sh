3e35817df6ad76a0a31944599ade02fd8174abd5
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-06 19:19:00 +0700

    fix code

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -4,7 +4,8 @@ echo "Start Downloading Misc files !"
 echo "Current Path: $PWD"
 
 # setup login password
-#sed -i '/reboot/ i\echo -e "bluedragon12\nbluedragon12" | passwd' files/etc/uci-defaults/99-init-settings.sh
+#password=bluedragon
+#sed -i '/reboot/ i\echo -e "$password\n$password" | passwd' files/etc/uci-defaults/99-init-settings.sh
 
 # custom script files urls
 ARCHH=$( [ "$TARGET" == "rpi-4" ] && echo "aarch64" || echo "x86_64" )
@@ -16,6 +17,7 @@ urls=("https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-$ARCHH.
       "https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/sbin/repair_ro"
       "https://raw.githubusercontent.com/frizkyiman/auto-mount-hdd/main/mount_hdd")
 
+{
 echo "setup 99-init-settings.sh"
 if [[ -e "files/etc/uci-defaults/99-init-settings_"$BASE""$branch_tag.sh"" ]]; then
      mv "files/etc/uci-defaults/99-init-settings_"$BASE""$branch_tag.sh"" "files/etc/uci-defaults/99-init-settings.sh"
@@ -27,7 +29,8 @@ if [[ -e "files/www/luci-static/resources/view/status/include/10_system_$BASE.js
      mv "files/www/luci-static/resources/view/status/include/10_system_$BASE.js" "files/www/luci-static/resources/view/status/include/10_system.js"
      rm files/www/luci-static/resources/view/status/include/10_system_*.js
 fi
-
+}
+{
 echo "Downloading custom script"
 mkdir -p files/etc/init.d
 mkdir -p files/sbin/
@@ -40,7 +43,9 @@ if wget --no-check-certificate -nv -P files "${urls[@]}"; then
     mv files/mount_hdd files/usr/bin/mount_hdd
     tar -zxf files/ookla-speedtest-1.2.0-linux-$ARCHH.tgz -C files/usr/bin && rm files/ookla-speedtest-1.2.0-linux-$ARCHH.tgz && rm files/usr/bin/speedtest.md
 fi
+}
 
+{
 cat <<'EOF' >files/etc/init.d/repair_ro
 #!/bin/sh /etc/rc.common
 
@@ -50,19 +55,22 @@ start() {
     /sbin/repair_ro
 }
 EOF
-
+}
+{
 cat <<'EOF' >files/usr/bin/repair_ro
 #!/bin/sh
 root_device="$1"
 /sbin/repair_ro "$root_device"
 EOF
-
+}
+{
 cat <<'EOF' >files/etc/config/vnstat
 config vnstat
 	list interface 'br-lan'
 	list interface 'wwan0'
 EOF
-
+}
+{
 sed -i '/reboot/ i\chmod +x /root/fix-tinyfm.sh && bash /root/fix-tinyfm.sh' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\chmod +x /sbin/sync_time.sh' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\chmod +x /sbin/free.sh' files/etc/uci-defaults/99-init-settings.sh
@@ -78,5 +86,5 @@ sed -i '/reboot/ i\chmod +x /usr/bin/enable-agh' files/etc/uci-defaults/99-init-
 sed -i '/reboot/ i\chmod +x /usr/bin/disable-agh' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\bash /usr/bin/enable-agh' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\uci set luci.main.mediaurlbase='/luci-static/argon' && uci commit' files/etc/uci-defaults/99-init-settings.sh
-
-echo "Download and configuration completed!"
+}
+echo "Downloading and configurating completed!"
