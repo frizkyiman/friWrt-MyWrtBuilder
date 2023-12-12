470bc15aaa2155e45271732d676206abf010c1ab
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-12 21:14:37 +0700

    using api to download latest core

--- scripts/agh-core.sh
+++ scripts/agh-core.sh
@@ -3,14 +3,15 @@
 echo "Start AdGuardHome Core Download !"
 echo "Current Path: $PWD"
 
-repo_url="https://github.com/AdguardTeam/AdGuardHome/releases"
-latest_version=$(curl -sSL "$repo_url/latest" | grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' | head -n 1)
-url="$repo_url/download/$latest_version/AdGuardHome_linux_$ARCH.tar.gz"
+agh_api="https://api.github.com/repos/AdguardTeam/AdGuardHome/releases" 
+agh_file="AdGuardHome_linux_$ARCH"
+agh_file_down="$(curl -s ${agh_api} | grep "browser_download_url" | grep -oE "https.*${agh_file}.*.tar.gz" | head -n 1)"
+latest_version=$(curl -sSL "$agh_api/latest" | grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' | head -n 1)
 
 cd files/opt || { echo "AdGuardHome core path does not exist!"; exit 1; }
 
-if wget -nv "$url"; then
+if wget -nv "$agh_file_down"; then
   echo "extracting core"
   tar -zxf "AdGuardHome_linux_$ARCH.tar.gz" && rm "AdGuardHome_linux_$ARCH.tar.gz"
-  echo "Done! AdGuardHome version $latest_version"
+  echo "Done! installed AdGuardHome version $latest_version"
 fi
