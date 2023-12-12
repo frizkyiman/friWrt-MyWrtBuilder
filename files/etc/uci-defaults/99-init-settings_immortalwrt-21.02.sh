9754beddc7a8eebec94a6895adc94def644e43ee
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-12 20:12:24 +0700

    fix typo

--- files/etc/uci-defaults/99-init-settings_immortalwrt-21.02.sh
+++ files/etc/uci-defaults/99-init-settings_immortalwrt-21.02.sh
@@ -105,7 +105,7 @@ echo '0 12 * * * /sbin/sync_time.sh beacon.liveon.id' >> /etc/crontabs/root
 /etc/init.d/cron restart
 
 mv /usr/share/openclash/ui/yacd /usr/share/openclash/ui/yacd.old && mv /usr/share/openclash/ui/yacd.new /usr/share/openclash/ui/yacd
-bash usr/bin/patchoc.sh
+bash /usr/bin/patchoc.sh
 
 echo -e "\ndtparam=i2c1=on\ndtparam=spi=on\ndtparam=i2s=on" >> /boot/config.txt
 sed -i "s/\(DISTRIB_DESCRIPTION='ImmortalWrt [0-9]*\.[0-9]*\.[0-9]*\).*'/\1'/g" /etc/openwrt_release
@@ -114,8 +114,6 @@ uci set nlbwmon.@nlbwmon[0].database_directory='/etc/nlbwmon'
 uci set nlbwmon.@nlbwmon[0].commit_interval='3h'
 uci set nlbwmon.@nlbwmon[0].refresh_interval='60s'
 uci commit nlbwmon
-
-sed -i "s/;DatabaseDir "/var/lib/vnstat"/DatabaseDir "/etc/vnstat"/" /etc/config/vnstat
 
 reboot
 
