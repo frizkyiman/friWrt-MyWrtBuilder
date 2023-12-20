ef3945252736e130c9c309efd3feb2132e5dad3f
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-20 21:21:37 +0700

    small c

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -80,8 +80,6 @@ sed -i '/reboot/ i\chmod +x /usr/bin/repair_ro' files/etc/uci-defaults/99-init-s
 sed -i '/reboot/ i\chmod +x /sbin/repair_ro' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\chmod +x /usr/bin/mount_hdd' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\chmod +x /usr/bin/speedtest' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /usr/bin/adguardhome' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\bash /usr/bin/adguardhome --force-enable' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\chmod +x /etc/init.d/vnstat_backup' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\bash /etc/init.d/vnstat_backup enable' files/etc/uci-defaults/99-init-settings.sh
 sed -i '/reboot/ i\uci set luci.main.mediaurlbase='/luci-static/argon' && uci commit' files/etc/uci-defaults/99-init-settings.sh
