f82a2d39e04ea9fe568fb145d8712c632c6d6b57
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-20 22:25:51 +0700

    optimizing code

--- scripts/agh-core.sh
+++ scripts/agh-core.sh
@@ -5,13 +5,17 @@ echo "Current Path: $PWD"
 
 agh_api="https://api.github.com/repos/AdguardTeam/AdGuardHome/releases" 
 agh_file="AdGuardHome_linux_$ARCH"
-agh_file_down="$(curl -s ${agh_api} | grep "browser_download_url" | grep -oE "https.*${agh_file}.*.tar.gz" | head -n 1)"
+agh_file_down="$(curl -s ${agh_api}/latest | grep "browser_download_url" | grep -oE "https.*${agh_file}.*.tar.gz" | head -n 1)"
 latest_version=$(curl -sSL "$agh_api/latest" | grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' | head -n 1)
 
-cd files/opt || { echo "AdGuardHome core path does not exist!"; exit 1; }
-
-if wget -nv "$agh_file_down"; then
-  echo "extracting core"
-  tar -zxf "AdGuardHome_linux_$ARCH.tar.gz" && rm "AdGuardHome_linux_$ARCH.tar.gz"
-  echo "Done! installed AdGuardHome version $latest_version"
+if wget -nv "$agh_file_down" -P files/opt; then
+  echo "Extracting core"
+  if tar -zxvf files/opt/AdGuardHome_linux_"$ARCH".tar.gz; then
+    rm files/opt/AdGuardHome_linux_"$ARCH".tar.gz
+    echo "Done! Installed AdGuardHome version $latest_version"
+  else
+    echo "Error: Failed to extract AdGuardHome."
+  fi
+else
+  echo "Error: Failed to download AdGuardHome."
 fi
