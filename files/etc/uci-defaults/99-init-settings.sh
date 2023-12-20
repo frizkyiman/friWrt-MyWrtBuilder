b6b008bbe49a28c5b5d06276e9170115f818631c
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-20 21:47:46 +0700

    optimizing and add some details

--- files/etc/uci-defaults/99-init-settings.sh
+++ files/etc/uci-defaults/99-init-settings.sh
@@ -2,6 +2,15 @@
 
 exec > /root/setup.log 2>&1
 
+# dont remove!
+if grep -q "ImmortalWrt" /etc/openwrt_release; then
+    sed -i "s/\(DISTRIB_DESCRIPTION='ImmortalWrt [0-9]*\.[0-9]*\.[0-9]*\).*'/\1'/g" /etc/openwrt_release
+    echo Branch version: "$(grep 'DISTRIB_DESCRIPTION=' /etc/openwrt_release | awk -F"'" '{print $2}')"
+elif grep -q "OpenWrt" /etc/openwrt_release; then
+    sed -i "s/\(DISTRIB_DESCRIPTION='OpenWrt [0-9]*\.[0-9]*\.[0-9]*\).*'/\1'/g" /etc/openwrt_release
+    echo Branch version: "$(grep 'DISTRIB_DESCRIPTION=' /etc/openwrt_release | awk -F"'" '{print $2}')"
+fi
+
 # Set hostname and Timezone to Asia/Jakarta
 uci set system.@system[0].timezone='WIB-7'
 uci set system.@system[0].zonename='Asia/Jakarta'
@@ -11,7 +20,7 @@ uci add_list system.ntp.server="id.pool.ntp.org"
 uci add_list system.ntp.server="time.google.com"
 uci commit system
 
-#configure wan interface
+# configure wan interface
 uci set network.wan=interface 
 uci set network.wan.proto='modemmanager'
 uci set network.wan.device='/sys/devices/platform/scb/fd500000.pcie/pci0000:00/0000:00:00.0/0000:01:00.0/usb2/2-1'
@@ -22,6 +31,7 @@ uci set network.lan.ipaddr="192.168.1.1"
 uci commit network
 /etc/init.d/network restart
 
+# configure ipv6
 uci -q delete dhcp.lan.dhcpv6
 uci -q delete dhcp.lan.ra
 uci delete dhcp.lan.ndp
@@ -31,7 +41,7 @@ uci commit dhcp
 uci set firewall.@zone[1].network='wan'
 uci commit firewall
 
-#configure WLAN
+# configure WLAN
 uci set wireless.@wifi-iface[0].mode='ap'
 uci set wireless.@wifi-device[0].disabled='0'
 uci set wireless.@wifi-device[0].country='ID'
@@ -70,7 +80,7 @@ echo '30 3 * * 0 echo  AT^RESET | atinout - /dev/ttyUSB0 - && sleep 20 && ifdown
 uci delete watchcat.@watchcat[0]
 uci commit
 
-#setting firewall samba4
+# setting firewall for samba4
 uci -q delete firewall.samba_nsds_nt
 uci set firewall.samba_nsds_nt="rule"
 uci set firewall.samba_nsds_nt.name="NoTrack-Samba/NS/DS"
@@ -97,45 +107,63 @@ uci set firewall.samba_smb_nt.proto="tcp"
 uci set firewall.samba_smb_nt.target="NOTRACK"
 uci commit firewall
 
+# set argon as default theme
+uci set luci.main.mediaurlbase='/luci-static/argon' && uci commit
+
+# remove login password required when accessing terminal
 uci set ttyd.@ttyd[0].command='/bin/bash --login'
 uci commit
 
+# setup nlbwmon database dir
+uci set nlbwmon.@nlbwmon[0].database_directory='/etc/nlbwmon'
+uci set nlbwmon.@nlbwmon[0].commit_interval='3h'
+uci set nlbwmon.@nlbwmon[0].refresh_interval='60s'
+uci commit nlbwmon
+
+# setup vnstat database dir
+sed -i 's/DatabaseDir "\/var\/lib\/vnstat"/DatabaseDir "\/etc\/vnstat"/g' /etc/vnstat.conf
+chmod +x /etc/init.d/vnstat_backup
+bash /etc/init.d/vnstat_backup enable
+
+# adjusting app catagory
 sed -i 's/services/nas/g' /usr/lib/lua/luci/controller/aria2.lua || sed -i 's/services/nas/g' /usr/share/luci/menu.d/luci-app-aria2.json
 sed -i 's/services/nas/g' /usr/share/luci/menu.d/luci-app-samba4.json
 sed -i 's/services/nas/g' /usr/share/luci/menu.d/luci-app-hd-idle.json
 sed -i 's/services/nas/g' /usr/share/luci/menu.d/luci-app-disks-info.json
 sed -i 's/services/status/g' /usr/share/luci/menu.d/luci-app-log.json
 
+# setup misc settings
 sed -i '/exit 0/i /usr/bin/patchoc.sh' /etc/rc.local
 sed -i 's/\[ -f \/etc\/banner \] && cat \/etc\/banner/#&/' /etc/profile
 sed -i 's/\[ -n "$FAILSAFE" \] && cat \/etc\/banner.failsafe/& || \/usr\/bin\/neofetch/' /etc/profile
 
 echo '*/15 * * * * /sbin/free.sh' >> /etc/crontabs/root
 echo '0 12 * * * /sbin/sync_time.sh beacon.liveon.id' >> /etc/crontabs/root
-/etc/init.d/cron restart
 
+chmod +x /root/fix-tinyfm.sh && bash /root/fix-tinyfm.sh
+chmod +x /sbin/sync_time.sh
+chmod +x /sbin/free.sh
+chmod +x /usr/bin/neofetch
+chmod +x /usr/bin/clock
+chmod +x /etc/init.d/repair_ro
+chmod +x /usr/bin/repair_ro
+chmod +x /sbin/repair_ro
+chmod +x /usr/bin/mount_hdd
+chmod +x /usr/bin/speedtest
+
+# configurating openclash
 mv /usr/share/openclash/ui/yacd /usr/share/openclash/ui/yacd.old && mv /usr/share/openclash/ui/yacd.new /usr/share/openclash/ui/yacd
 bash /usr/bin/patchoc.sh
+chmod +x /etc/openclash/core/clash
+chmod +x /etc/openclash/core/clash_tun
+chmod +x /etc/openclash/core/clash_meta
 
+# adding new line for enable i2c oled display
 echo -e "\ndtparam=i2c1=on\ndtparam=spi=on\ndtparam=i2s=on" >> /boot/config.txt
 
-# dont remove!
-if grep -q "ImmortalWrt" /etc/openwrt_release; then
-    sed -i "s/\(DISTRIB_DESCRIPTION='ImmortalWrt [0-9]*\.[0-9]*\.[0-9]*\).*'/\1'/g" /etc/openwrt_release
-    echo "ImmortalWrt version updated."
-elif grep -q "OpenWrt" /etc/openwrt_release; then
-    sed -i "s/\(DISTRIB_DESCRIPTION='OpenWrt [0-9]*\.[0-9]*\.[0-9]*\).*'/\1'/g" /etc/openwrt_release
-    echo "OpenWrt version updated."
-else
-    echo "No matching distribution found in /etc/openwrt_release."
-fi
-
-uci set nlbwmon.@nlbwmon[0].database_directory='/etc/nlbwmon'
-uci set nlbwmon.@nlbwmon[0].commit_interval='3h'
-uci set nlbwmon.@nlbwmon[0].refresh_interval='60s'
-uci commit nlbwmon
-
-sed -i 's/DatabaseDir "\/var\/lib\/vnstat"/DatabaseDir "\/etc\/vnstat"/g' /etc/vnstat.conf
+# enable adguardhome
+chmod +x /usr/bin/adguardhome
+bash /usr/bin/adguardhome enable
 
 reboot
 
