becb48104e52a075e6860aaa09d31c501a1e59a5
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-20 21:55:11 +0700

    optimizing code

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -6,10 +6,12 @@ echo "Current Path: $PWD"
 #setup custom setting for openwrt or immortalwrt
 {
 if [[ "${RELEASE_BRANCH%:*}" == "openwrt" ]]; then
+    echo "$RELEASE_BRANCH"
     sed -i '/reboot/ i\mv \/www\/luci-static\/resources\/view\/status\/include\/29_temp.js \/www\/luci-static\/resources\/view\/status\/include\/17_temp.js' files/etc/uci-defaults/99-init-settings.sh
     mv "files/www/luci-static/resources/view/status/include/10_system_openwrt.js" "files/www/luci-static/resources/view/status/include/10_system.js"
     rm files/www/luci-static/resources/view/status/include/10_system_immortalwrt.js
 elif [[ "${RELEASE_BRANCH%:*}" == "immortalwrt" ]]; then
+    echo "$RELEASE_BRANCH"
     if [[ "$(echo "${RELEASE_BRANCH#*:}" | awk -F '.' '{print $1"."$2}')" == "23.05" ]]; then
         cp packages/luci-app-oled_1.0_all.ipk files/root/luci-app-oled_1.0_all.ipk
         sed -i '/reboot/ i\opkg install /root/luci-app-oled_1.0_all.ipk --force-reinstall' files/etc/uci-defaults/99-init-settings.sh
@@ -38,7 +40,6 @@ if [ -n "$WIFI_PASSWORD" ]; then
 else
     echo "Wifi password is not set, skipping..."
 fi
-
 sed -i "/#configure WLAN/ a\uci set wireless.@wifi-iface[0].ssid=\"$WIFI_SSID\"" files/etc/uci-defaults/99-init-settings.sh
 }
 
@@ -46,42 +47,20 @@ sed -i "/#configure WLAN/ a\uci set wireless.@wifi-iface[0].ssid=\"$WIFI_SSID\""
 echo "Downloading custom script" 
 # custom script files urls
 urls=("https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-$ARCH_2.tgz"
-      "https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch"
       "https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/sbin/sync_time.sh"
       "https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/usr/bin/clock"
       "https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/sbin/repair_ro"
       "https://raw.githubusercontent.com/frizkyiman/auto-mount-hdd/main/mount_hdd")
 
-mkdir -p files/etc/init.d
-mkdir -p files/sbin/
 if wget --no-check-certificate -nv -P files "${urls[@]}"; then
-    echo "sync && echo 3 > /proc/sys/vm/drop_caches && rm -rf /tmp/luci*" >> files/sbin/free.sh
     mv files/sync_time.sh files/sbin/sync_time.sh
-    mv files/neofetch files/usr/bin/neofetch
     mv files/clock files/usr/bin/clock
     mv files/repair_ro files/sbin/repair_ro
     mv files/mount_hdd files/usr/bin/mount_hdd
     tar -zxf files/ookla-speedtest-1.2.0-linux-$ARCH_2.tgz -C files/usr/bin && rm files/ookla-speedtest-1.2.0-linux-$ARCH_2.tgz && rm files/usr/bin/speedtest.md
+else
+    echo "Failed to download and configuring custom script"
 fi
 }
 
-{
-sed -i '/reboot/ i\chmod +x /etc/openclash/core/clash' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /etc/openclash/core/clash_tun' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /etc/openclash/core/clash_meta' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /root/fix-tinyfm.sh && bash /root/fix-tinyfm.sh' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /sbin/sync_time.sh' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /sbin/free.sh' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /usr/bin/patchoc.sh' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /usr/bin/neofetch' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /usr/bin/clock' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /etc/init.d/repair_ro' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /usr/bin/repair_ro' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /sbin/repair_ro' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /usr/bin/mount_hdd' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /usr/bin/speedtest' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\chmod +x /etc/init.d/vnstat_backup' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\bash /etc/init.d/vnstat_backup enable' files/etc/uci-defaults/99-init-settings.sh
-sed -i '/reboot/ i\uci set luci.main.mediaurlbase='/luci-static/argon' && uci commit' files/etc/uci-defaults/99-init-settings.sh
-}
-echo "Download and configurating completed!"
+echo "All configuration setup completed!"
