ba37a2ca13650e06d5e7a6913abbb7521de5c796
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-30 22:56:37 +0700

    small fix

--- make-build.sh
+++ make-build.sh
@@ -28,7 +28,7 @@ PACKAGES="$PACKAGES kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 kmod-usb-net-asix 
 OPENCLASH_FW3="$PACKAGES coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun unzip luci-compat luci luci-base luci-app-openclash"
 OPENCLASH_FW4="$PACKAGES coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base luci-app-openclash"
 PASSWALL="$PACKAGES coreutils-base64 resolveip dns2socks dns2tcp microsocks tcping brook hysteria trojan-go xray-core xray-plugin sing-box chinadns-ng luci-app-passwall"
-if [ "$TUNNEL" == "openclash_passwall" ]; then
+if [ "$TUNNEL" == "openclash-passwall" ]; then
     PACKAGES="$([ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ] && echo "$OPENCLASH_FW3" || echo "$OPENCLASH_FW4") $PASSWALL"
 elif [ "$TUNNEL" == "openclash" ]; then
     PACKAGES="$([ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ] && echo "$OPENCLASH_FW3" || echo "$OPENCLASH_FW4")"
