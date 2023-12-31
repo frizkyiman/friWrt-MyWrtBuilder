070e0eafa7d05c402205d736167dbf68bad6063d
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-31 20:36:59 +0700

    optimizing code

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -10,7 +10,7 @@ if [[ "${RELEASE_BRANCH%:*}" == "openwrt" ]]; then
     sed -i '/reboot/ i\mv \/www\/luci-static\/resources\/view\/status\/include\/29_temp.js \/www\/luci-static\/resources\/view\/status\/include\/17_temp.js' files/etc/uci-defaults/99-init-settings.sh
 elif [[ "${RELEASE_BRANCH%:*}" == "immortalwrt" ]]; then
     echo "$RELEASE_BRANCH"
-    if [[ "$(echo "${RELEASE_BRANCH#*:}" | awk -F '.' '{print $1"."$2}')" == "23.05" ]]; then
+    if [[ "$(echo "${RELEASE_BRANCH#*:}" | awk -F '.' '{print $1}')" == "23" ]]; then
         cp packages/luci-app-oled_1.0_all.ipk files/root/luci-app-oled_1.0_all.ipk
         sed -i '/reboot/ i\opkg install /root/luci-app-oled_1.0_all.ipk --force-reinstall' files/etc/uci-defaults/99-init-settings.sh
         sed -i '/reboot/ i\rm /root/luci-app-oled_1.0_all.ipk' files/etc/uci-defaults/99-init-settings.sh
@@ -39,24 +39,26 @@ fi
 sed -i "/#configure WLAN/ a\uci set wireless.@wifi-iface[0].ssid=\"$WIFI_SSID\"" files/etc/uci-defaults/99-init-settings.sh
 }
 
+# custom script files urls
 {
 echo "Downloading custom script" 
-# custom script files urls
-urls=("https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-$ARCH_2.tgz"
-      "https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/sbin/sync_time.sh"
-      "https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/usr/bin/clock"
-      "https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/sbin/repair_ro"
-      "https://raw.githubusercontent.com/frizkyiman/auto-mount-hdd/main/mount_hdd")
+speedtest="https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-$ARCH_2.tgz"
+sync_time="https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/sbin/sync_time.sh"
+clock="https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/usr/bin/clock"
+repair_ro="https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/sbin/repair_ro"
+mount_hdd="https://raw.githubusercontent.com/frizkyiman/auto-mount-hdd/main/mount_hdd"
 
-if wget --no-check-certificate -nv -P files "${urls[@]}"; then
-    mv files/sync_time.sh files/sbin/sync_time.sh
-    mv files/clock files/usr/bin/clock
-    mv files/repair_ro files/sbin/repair_ro
-    mv files/mount_hdd files/usr/bin/mount_hdd
-    tar -zxf files/ookla-speedtest-1.2.0-linux-$ARCH_2.tgz -C files/usr/bin && rm files/ookla-speedtest-1.2.0-linux-$ARCH_2.tgz && rm files/usr/bin/speedtest.md
+if wget --no-check-certificate -nv -P files "$speedtest"; then
+   tar -zxf files/ookla-speedtest-1.2.0-linux-"$ARCH_2".tgz -C files/usr/bin
+   rm files/ookla-speedtest-1.2.0-linux-"$ARCH_2".tgz && rm files/usr/bin/speedtest.md
 else
-    echo "Failed to download and configuring custom script"
+    echo "Failed to download and extract speedtest files!"
 fi
+
+wget --no-check-certificate -nv -P files/sbin "$sync_time"
+wget --no-check-certificate -nv -P files/usr/bin "$clock"
+wget --no-check-certificate -nv -P files/sbin "$repair_ro"
+wget --no-check-certificate -nv -P files/usr/bin "$mount_hdd"
 }
 
-echo "All configuration setup completed!"
+echo "All custom configuration setup completed!"
