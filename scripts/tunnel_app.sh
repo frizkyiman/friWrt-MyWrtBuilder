dfdfe2d823e83d1262666ef2b3ea1704edfd4d1c
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-12-31 22:20:52 +0700

    update url

--- scripts/tunnel_app.sh
+++ scripts/tunnel_app.sh
@@ -3,23 +3,25 @@
 openclash_api="https://api.github.com/repos/vernesong/OpenClash/releases"
 openclash_file="luci-app-openclash"
 openclash_file_down="$(curl -s ${openclash_api} | grep "browser_download_url" | grep -oE "https.*${openclash_file}.*.ipk" | head -n 1)"
-passwall_packages="https://github.com/xiaorouji/openwrt-passwall/releases/download/4.71-2/passwall_packages_ipk_$ARCH_3.zip"
-passwall_packages_file="passwall_packages_ipk_$ARCH_3.zip"
+
+passwall_api="https://api.github.com/repos/xiaorouji/openwrt-passwall2/releases"
+passwall_file="passwall2_packages_ipk_$ARCH_3.zip"
+passwall_file_down="$(curl -s ${passwall_api} | grep "browser_download_url" | grep -oE "https.*${passwall_file}" | head -n 1)"
 
 if [ "$TUNNEL" == "openclash" ]; then
     echo "Downloading Openclash packages"
     wget ${openclash_file_down} -nv -P packages
 elif [ "$TUNNEL" == "passwall" ]; then
-    echo "Downloading Passwall packages"
-    wget "$passwall_packages" -nv -P packages
-    unzip -qq packages/"$passwall_packages_file" -d packages && rm packages/"$passwall_packages_file"
+    echo "Downloading Passwall packages ipk"
+    wget "$passwall_file_down" -nv -P packages
+    unzip -qq packages/"$passwall_file" -d packages && rm packages/"$passwall_file"
 elif [ "$TUNNEL" == "openclash-passwall" ]; then
     echo "Installing Openclash and Passwall"
     echo "Downloading Openclash packages"
     wget ${openclash_file_down} -nv -P packages
-    echo "Downloading Passwall packages"
-    wget "$passwall_packages" -nv -P packages
-    unzip -qq packages/"$passwall_packages_file" -d packages && rm packages/"$passwall_packages_file"
+    echo "Downloading Passwall packages ipk"
+    wget "$passwall_file_down" -nv -P packages
+    unzip -qq packages/"$passwall_file" -d packages && rm packages/"$passwall_file"
 fi 
 if [ "$?" -ne 0 ]; then
     echo "Error: Download or extraction failed."
