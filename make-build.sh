0bef2e0cb09cb40c5c33479999e9a0b16e7c2196
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2024-01-01 22:18:59 +0700

    update passwall depedencies

--- make-build.sh
+++ make-build.sh
@@ -27,7 +27,7 @@ PACKAGES="$PACKAGES kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 kmod-usb-net-asix 
 # tunnel option
 OPENCLASH_FW3="$PACKAGES coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun unzip luci-compat luci luci-base luci-app-openclash"
 OPENCLASH_FW4="$PACKAGES coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base luci-app-openclash"
-PASSWALL="$PACKAGES coreutils-base64 resolveip dns2socks dns2tcp microsocks tcping brook hysteria trojan-go xray-core xray-plugin sing-box chinadns-ng luci-app-passwall"
+PASSWALL="$PACKAGES ipset ipt2socks iptables iptables-legacy iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy kmod-ipt-nat coreutils coreutils-base64 coreutils-nohup curl dns2socks ip-full libuci-lua lua luci-compat luci-lib-jsonc microsocks resolveip tcping unzip dns2tcp brook chinadns-ng haproxy hysteria ip6tables-mod-nat kcptun-client naiveproxy pdnsd-alt shadowsocks-libev-ss-local shadowsocks-libev-ss-redir shadowsocks-libev-ss-server shadowsocks-rust-sslocal shadowsocksr-libev-ssr-local shadowsocksr-libev-ssr-redir shadowsocksr-libev-ssr-server simple-obfs trojan-go trojan-plus v2ray-core v2ray-plugin xray-core xray-plugin"
 if [ "$TUNNEL" == "openclash-passwall" ]; then
     PACKAGES="$([ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ] && echo "$OPENCLASH_FW3" || echo "$OPENCLASH_FW4") $PASSWALL"
 elif [ "$TUNNEL" == "openclash" ]; then
