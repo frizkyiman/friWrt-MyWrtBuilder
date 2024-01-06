fcb91e08acaf9d017dff0ce263f24f0dfce31d48
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2024-01-06 23:56:13 +0700

    remove unused command.

--- files/root/fix-tinyfm.sh
+++ files/root/fix-tinyfm.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-chmod -R 755 /usr/lib/lua/luci/controller/*
-chmod -R 755 /usr/lib/lua/luci/view/*
-chmod -R 755 /www/*
-chmod -R 755 /www/tinyfm/*
-chmod -R 755 /www/tinyfm/assets/*
 # Autofix download index.php, index.html
 if ! grep -q ".php=/usr/bin/php-cgi" /etc/config/uhttpd; then
 	echo -e "  helmilog : system not using php-cgi, patching php config ..."
