1bca776496a33405b937c69286f26d0fb03449bb
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2024-01-06 14:43:31 +0700

    modify some logic for packages

--- make-build.sh
+++ make-build.sh
@@ -19,7 +19,7 @@ kmod-usb-uhci kmod-usb2 kmod-usb-ehci kmod-usb-net-ipheth usbmuxd libusbmuxd-uti
 # Modem Tools
 PACKAGES="$PACKAGES luci-app-modeminfo atinout luci-app-modemband modemband luci-app-mmconfig sms-tool luci-app-sms-tool-js luci-app-lite-watchdog luci-app-3ginfo-lite luci-app-modemdefine picocom minicom"
 
-# tunnel option
+# Tunnel option
 OPENCLASH_FW3="$PACKAGES coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun unzip luci-compat luci luci-base luci-app-openclash"
 OPENCLASH_FW4="$PACKAGES coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base luci-app-openclash"
 PASSWALL="$PACKAGES ipset ipt2socks iptables iptables-legacy iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy kmod-ipt-nat coreutils coreutils-base64 coreutils-nohup curl dns2socks ip-full libuci-lua lua luci-compat luci-lib-jsonc microsocks resolveip tcping unzip dns2tcp brook hysteria trojan-go xray-core xray-plugin sing-box chinadns-ng haproxy ip6tables-mod-nat kcptun-client naiveproxy pdnsd-alt shadowsocks-libev-ss-local shadowsocks-libev-ss-redir shadowsocks-libev-ss-server shadowsocks-rust-sslocal shadowsocksr-libev-ssr-local shadowsocksr-libev-ssr-redir shadowsocksr-libev-ssr-server simple-obfs trojan-plus v2ray-core v2ray-plugin luci-app-passwall"
@@ -47,9 +47,6 @@ PACKAGES="$PACKAGES luci-app-internet-detector internet-detector nlbwmon luci-ap
 # Argon Theme
 PACKAGES="$PACKAGES luci-theme-argon luci-app-argon-config"
 
-# i2c tools
-PACKAGES="$PACKAGES i2c-tools kmod-i2c-core kmod-i2c-gpio luci-app-oled"
-
 # PHP8
 PACKAGES="$PACKAGES libc php8 php8-fastcgi php8-fpm php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv php8-mod-mbstring coreutils-stat zoneinfo-asia"
 
@@ -59,11 +56,13 @@ if [ "${RELEASE_BRANCH%:*}" == "openwrt" ]; then
 elif [ "${RELEASE_BRANCH%:*}" == "immortalwrt" ]; then
     misc=""
 fi
-CUSTOM=$( [ "$PROFILE" == "rpi-4" ] && echo "kmod-i2c-bcm2835" )
-PACKAGES="$PACKAGES $misc $CUSTOM zram-swap adb parted losetup resize2fs luci luci-ssl block-mount luci-app-poweroff iperf3 luci-app-log luci-app-ramfree htop luci-app-watchcat bash curl tar unzip unrar jq luci-app-ttyd nano git git-http"
 
-# Custom Files
-FILES="files"
+if[ "$PROFILE" == "rpi-4" ]; then
+    misc="kmod-i2c-bcm2835 i2c-tools kmod-i2c-core kmod-i2c-gpio luci-app-oled"
+if[ "$PROFILE" == "generic" ]; then
+    misc=""
+fi
+PACKAGES="$PACKAGES $misc zram-swap adb parted losetup resize2fs luci luci-ssl block-mount luci-app-poweroff iperf3 luci-app-log luci-app-ramfree htop luci-app-watchcat bash curl tar unzip unrar jq luci-app-ttyd nano git git-http"
 
 # Exclude package (must use - before packages name)
 if [ "${RELEASE_BRANCH%:*}" == "openwrt" ]; then
@@ -71,6 +70,9 @@ if [ "${RELEASE_BRANCH%:*}" == "openwrt" ]; then
 elif [ "${RELEASE_BRANCH%:*}" == "immortalwrt" ]; then
     EXCLUDED="-dnsmasq -automount -libustream-openssl -default-settings-chn -luci-i18n-base-zh-cn"
 fi
+
+# Custom Files
+FILES="files"
 
 # Disable service
 DISABLED_SERVICES="AdGuardHome"
