7328e4a0dee0647304e52319cab5a2cdb7f7461f
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2024-01-08 19:30:21 +0700

    Update clash-core.sh

--- scripts/clash-core.sh
+++ scripts/clash-core.sh
@@ -14,15 +14,6 @@ else
    echo "Failed to download YACD Dashboard"
 fi
 
-sed -i "/# configurating openclash/ a\mv /usr/share/openclash/ui/yacd /usr/share/openclash/ui/yacd.old" files/etc/uci-defaults/99-init-settings.sh
-sed -i "/# configurating openclash/ a\mv /usr/share/openclash/ui/yacd.new /usr/share/openclash/ui/yacd" files/etc/uci-defaults/99-init-settings.sh
-sed -i "/# configurating openclash/ a\bash /usr/bin/patchoc.sh" files/etc/uci-defaults/99-init-settings.sh
-sed -i "/# configurating openclash/ a\chmod +x /etc/openclash/core/clash" files/etc/uci-defaults/99-init-settings.sh
-sed -i "/# configurating openclash/ a\chmod +x /etc/openclash/core/clash_tun" files/etc/uci-defaults/99-init-settings.sh
-sed -i "/# configurating openclash/ a\chmod +x /etc/openclash/core/clash_meta" files/etc/uci-defaults/99-init-settings.sh
-sed -i "/# configurating openclash/ a\chmod +x /usr/bin/patchoc.sh" files/etc/uci-defaults/99-init-settings.sh
-sed -i "/# configurating openclash/ a\sed -i '\/exit 0\/i # \/usr\/bin\/patchoc.sh' /etc/rc.local" files/etc/uci-defaults/99-init-settings.sh
-
 echo "Start Clash Core Download !"
 #core download url
 clash="https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-$ARCH_1.tar.gz"
