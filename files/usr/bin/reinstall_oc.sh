6b29834ebe53aab77636131dc66296c56930a88d
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2024-01-28 23:49:22 +0700

    add logic to check packages script needed for run

--- files/usr/bin/reinstall_oc.sh
+++ files/usr/bin/reinstall_oc.sh
@@ -3,6 +3,18 @@
 INFO="[\033[94m INFO \033[0m]"
 SUCCESS="[\033[92m SUCCESS \033[0m]"
 ERROR="[\033[91m ERROR \033[0m]"
+opkg_updated=false
+
+required_packages=("wget-ssl" "bash" "curl")
+for package in "${required_packages[@]}"; do
+    if ! opkg list-installed | grep -q "^$package -"; then
+        echo -e "${INFO} Package $package to initialize setup not found. Installing..."
+        [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
+        opkg install $package
+    else
+        echo -e "${INFO} $package Installed!."
+    fi
+done
 
 openclash_api="https://api.github.com/repos/vernesong/OpenClash/releases"
 openclash_file="luci-app-openclash"
@@ -14,10 +26,12 @@ if [ "$1" == "install" ]; then
   if opkg list-installed | grep -q '^dnsmasq\b'; then echo -e "${INFO} dnsmasq is already installed. Removing..."; opkg remove dnsmasq; fi
   if [ -n "$(command -v fw4)" ]; then
     echo -e "${INFO} Firewall 4 nftables detected"
-    opkg update && opkg install coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base
+    [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
+    opkg install coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base
   else
     echo -e "${INFO} Firewall 3 iptables detected"
-    opkg update && opkg install coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci luci-base
+    [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
+    opkg install coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci luci-base
   fi
 fi
 
@@ -25,7 +39,8 @@ echo -e "${INFO} Start downloading [ ${openclash_file} ]."
 if wget -q -N -P /root "${openclash_file_down}" && wget -q -N -P /usr/bin "$patchoc"; then
   echo -e "${SUCCESS} The [ $(basename "${openclash_file_down}") ] is downloaded successfully."
   echo -e "${INFO} Start installing [ ${openclash_file} ]"
-  if opkg update && opkg install /root/*openclash*.ipk --force-reinstall; then
+  [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
+  if opkg install /root/*openclash*.ipk --force-reinstall; then
     echo -e "${INFO} Start applying patch for [ ${openclash_file} ]."
     chmod +x /usr/bin/patchoc.sh && /usr/bin/patchoc.sh
     [ "${?}" -eq "0" ] && echo -e "${SUCCESS} Openclash successfully installed and patched." || echo -e "${ERROR} Failed to apply patch! Check for errors during patching."
