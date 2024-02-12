9e13d865f90ea02322ed9f173f32f44974d4e831
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2024-02-13 01:00:53 +0700

    fix wrong folder extract

--- scripts/clash-core.sh
+++ scripts/clash-core.sh
@@ -33,7 +33,7 @@ fi
 mkdir -p $core_dir
 echo "Downloading clash.tar.gz..."
 if wget --no-check-certificate -nv -O $core_dir/clash.tar.gz $clash; then
-   tar -zxf $core_dir/clash.tar.gz && rm $core_dir/clash.tar.gz
+   tar -zxf $core_dir/clash.tar.gz -C $core_dir && rm $core_dir/clash.tar.gz
    echo "clash.tar.gz downloaded and extracted successfully."
 else
    echo "Failed to download clash.tar.gz."
