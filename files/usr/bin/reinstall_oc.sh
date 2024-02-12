62ce1f691c44552b31cf2b657d22ea0d39404830
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2024-02-12 22:03:50 +0700

    adding script to install core

--- files/usr/bin/reinstall_oc.sh
+++ files/usr/bin/reinstall_oc.sh
@@ -22,32 +22,40 @@ openclash_file_down="$(curl -s "${openclash_api}" | grep "browser_download_url" 
 patchoc="https://raw.githubusercontent.com/frizkyiman/friWrt-MyWrtBuilder/main/files/usr/bin/patchoc.sh"
 
 if [ "$1" == "install" ]; then
-  echo -e "${INFO} Start installing [ ${openclash_file} ] dependencies first"
-  if opkg list-installed | grep -q '^dnsmasq\b'; then echo -e "${INFO} dnsmasq is already installed. Removing..."; opkg remove dnsmasq; fi
-  if [ -n "$(command -v fw4)" ]; then
-    echo -e "${INFO} Firewall 4 nftables detected"
-    [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
-    opkg install coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base
-  else
-    echo -e "${INFO} Firewall 3 iptables detected"
-    [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
-    opkg install coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci luci-base
-  fi
+    echo -e "${INFO} Start installing [ ${openclash_file} ] dependencies first"
+    if opkg list-installed | grep -q '^dnsmasq\b'; then echo -e "${INFO} dnsmasq is already installed. Removing..."; opkg remove dnsmasq; fi
+    if [ -n "$(command -v fw4)" ]; then
+        echo -e "${INFO} Firewall 4 nftables detected"
+        [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
+        opkg install coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base
+    else
+        echo -e "${INFO} Firewall 3 iptables detected"
+        [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
+        opkg install coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci luci-base
+    fi
+elif [ "$1" == "install" ] || [ "$1" == "install-core" ]; then
+    echo -e "${INFO} Start downloading core..."
+    yacd_dir="/usr/share/openclash/ui"
+    core_dir="/etc/openclash/core"
+    ARCH_1=$(uname -m) && { [ "$ARCH_1" == "aarch64" ] && ARCH_1="arm64" || [ "$ARCH_1" == "x86_64" ] && ARCH_1="amd64"; }
+    [ "$ARCH_1" == "x86_64" ] && PROFILE="generic"
+    bash -c "$(curl -fsSL https://github.com/frizkyiman/friWrt-MyWrtBuilder/raw/main/scripts/clash-core.sh)"
+    echo -e "${SUCCESS} Done!"
 fi
 
 echo -e "${INFO} Start downloading [ ${openclash_file} ]."
 if wget -q -N -P /root "${openclash_file_down}" && wget -q -N -P /usr/bin "$patchoc"; then
-  echo -e "${SUCCESS} The [ $(basename "${openclash_file_down}") ] is downloaded successfully."
-  echo -e "${INFO} Start installing [ ${openclash_file} ]"
-  [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
-  if opkg install /root/*openclash*.ipk --force-reinstall; then
-    echo -e "${INFO} Start applying patch for [ ${openclash_file} ]."
-    chmod +x /usr/bin/patchoc.sh && /usr/bin/patchoc.sh
-    [ "${?}" -eq "0" ] && echo -e "${SUCCESS} Openclash successfully installed and patched." || echo -e "${ERROR} Failed to apply patch! Check for errors during patching."
-  else
-    echo -e "${ERROR} Failed to install [ ${openclash_file} ]! Check for errors during installation."
-  fi
-  rm /root/*openclash*.ipk
+    echo -e "${SUCCESS} The [ $(basename "${openclash_file_down}") ] is downloaded successfully."
+    echo -e "${INFO} Start installing [ ${openclash_file} ]"
+    [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
+    if opkg install /root/*openclash*.ipk --force-reinstall; then
+        echo -e "${INFO} Start applying patch for [ ${openclash_file} ]."
+        chmod +x /usr/bin/patchoc.sh && /usr/bin/patchoc.sh
+        [ "${?}" -eq "0" ] && echo -e "${SUCCESS} Openclash successfully installed and patched." || echo -e "${ERROR} Failed to apply patch! Check for errors during patching."
+    else
+        echo -e "${ERROR} Failed to install [ ${openclash_file} ]! Check for errors during installation."
+    fi
+    rm /root/*openclash*.ipk
 else
-  echo -e "${ERROR} [ ${openclash_file} ] download failed! Make sure the connection is accessible."
+    echo -e "${ERROR} [ ${openclash_file} ] download failed! Make sure the connection is accessible."
 fi
