d51c73c3b6abd24d2c581ac1053c788d1c94e7cc
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2024-02-12 23:35:47 +0700

    fix logic order

--- files/usr/bin/reinstall_oc.sh
+++ files/usr/bin/reinstall_oc.sh
@@ -34,6 +34,24 @@ if [ "$1" == "install" ]; then
         opkg install coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci luci-base
     fi
 fi
+
+echo -e "${INFO} Start downloading [ ${openclash_file} ]."
+if wget -q -N -P /root "${openclash_file_down}" && wget -q -N -P /usr/bin "$patchoc"; then
+    echo -e "${SUCCESS} The [ $(basename "${openclash_file_down}") ] is downloaded successfully."
+    echo -e "${INFO} Start installing [ ${openclash_file} ]"
+    [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
+    if opkg install /root/*openclash*.ipk --force-reinstall; then
+        echo -e "${INFO} Start applying patch for [ ${openclash_file} ]."
+        chmod +x /usr/bin/patchoc.sh && /usr/bin/patchoc.sh
+        [ "${?}" -eq "0" ] && echo -e "${SUCCESS} Openclash successfully installed and patched." || echo -e "${ERROR} Failed to apply patch! Check for errors during patching."
+    else
+        echo -e "${ERROR} Failed to install [ ${openclash_file} ]! Check for errors during installation."
+    fi
+    rm /root/*openclash*.ipk
+else
+    echo -e "${ERROR} [ ${openclash_file} ] download failed! Make sure the connection is accessible."
+fi
+
 if [ "$1" == "install" ] || [ "$1" == "install-core" ]; then
     echo -e "${INFO} Start downloading core..."
     yacd_dir="/usr/share/openclash/ui"
@@ -54,21 +72,4 @@ if [ "$1" == "install" ] || [ "$1" == "install-core" ]; then
     chmod +x /usr/bin/patchoc.sh
     bash /usr/bin/patchoc.sh
     sed -i '/exit 0/i #/usr/bin/patchoc.sh' /etc/rc.local
-fi
-
-echo -e "${INFO} Start downloading [ ${openclash_file} ]."
-if wget -q -N -P /root "${openclash_file_down}" && wget -q -N -P /usr/bin "$patchoc"; then
-    echo -e "${SUCCESS} The [ $(basename "${openclash_file_down}") ] is downloaded successfully."
-    echo -e "${INFO} Start installing [ ${openclash_file} ]"
-    [ "$opkg_updated" = false ] && opkg update && opkg_updated=true
-    if opkg install /root/*openclash*.ipk --force-reinstall; then
-        echo -e "${INFO} Start applying patch for [ ${openclash_file} ]."
-        chmod +x /usr/bin/patchoc.sh && /usr/bin/patchoc.sh
-        [ "${?}" -eq "0" ] && echo -e "${SUCCESS} Openclash successfully installed and patched." || echo -e "${ERROR} Failed to apply patch! Check for errors during patching."
-    else
-        echo -e "${ERROR} Failed to install [ ${openclash_file} ]! Check for errors during installation."
-    fi
-    rm /root/*openclash*.ipk
-else
-    echo -e "${ERROR} [ ${openclash_file} ] download failed! Make sure the connection is accessible."
 fi
