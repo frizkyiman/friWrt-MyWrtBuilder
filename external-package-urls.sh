b7e6435bc00b3fd241cd8b46a6fef7952fb3fe44
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2024-02-19 22:22:58 +0700

    update downloaded files location

--- external-package-urls.sh
+++ external-package-urls.sh
@@ -11,10 +11,9 @@ files=(
 for entry in "${files[@]}"; do
     IFS="|" read -r filename base_url <<< "$entry"
     file_url=$(curl -sL "$base_url" | grep -o "$filename[_0-9a-zA-Z\.-]*\.ipk" | head -n 1)
-    
     if [ -n "$file_url" ]; then
         echo "Downloading $file_url from $base_url..."
-        curl -LO "$base_url/$file_url"
+        curl -Lo "packages/$file_url" "$base_url/$file_url"
         echo "Download complete."
     else
         echo "Failed to retrieve $filename filename from $base_url. Exiting."
