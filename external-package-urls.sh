09faad50b097362d396409e6d3ed1d5a40628ac2
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2024-02-23 00:20:10 +0700

    Update external-package-urls.sh

--- external-package-urls.sh
+++ external-package-urls.sh
@@ -2,22 +2,22 @@
 # This script for packages url from snapshots repo's
 # Put url base and file name.
 
-#files=(
+files=(
 #    "luci-proto-modemmanager|https://downloads.openwrt.org/snapshots/packages/$ARCH_3/luci"
 #    "modemmanager|https://downloads.openwrt.org/snapshots/packages/$ARCH_3/packages"
-#    "luci-proto-mbim|https://downloads.openwrt.org/snapshots/packages/$ARCH_3/luci"
-#    "sms-tool|https://downloads.openwrt.org/snapshots/packages/$ARCH_3/packages"
-#)
+    "luci-proto-mbim|https://downloads.openwrt.org/snapshots/packages/$ARCH_3/luci"
+    "sms-tool|https://downloads.openwrt.org/snapshots/packages/$ARCH_3/packages"
+)
 
-#for entry in "${files[@]}"; do
-#    IFS="|" read -r filename base_url <<< "$entry"
-#    file_url=$(curl -sL "$base_url" | grep -o "$filename[_0-9a-zA-Z\.-]*\.ipk" | head -n 1)
-#    if [ -n "$file_url" ]; then
-#        echo "Downloading $file_url from $base_url..."
-#        curl -Lo "packages/$file_url" "$base_url/$file_url"
-#        echo "Download complete."
-#    else
-#        echo "Failed to retrieve $filename filename from $base_url. Exiting."
-#        exit 1
-#    fi
-#done
+for entry in "${files[@]}"; do
+    IFS="|" read -r filename base_url <<< "$entry"
+    file_url=$(curl -sL "$base_url" | grep -o "$filename[_0-9a-zA-Z\.-]*\.ipk" | head -n 1)
+    if [ -n "$file_url" ]; then
+        echo "Downloading $file_url from $base_url..."
+        curl -Lo "packages/$file_url" "$base_url/$file_url"
+        echo "Download complete."
+    else
+        echo "Failed to retrieve $filename filename from $base_url. Exiting."
+        exit 1
+    fi
+done
