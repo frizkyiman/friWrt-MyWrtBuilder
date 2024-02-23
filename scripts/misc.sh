e45366760ba3e008a973d132598538b795c3b7c3
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2024-02-23 15:43:47 +0700

    Update misc.sh

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -4,6 +4,7 @@ echo "Start Downloading Misc files and setup configuration!"
 echo "Current Path: $PWD"
 
 #setup custom setting for openwrt and immortalwrt
+sed -i "s/Ouc3kNF6/$DATE/g" files/etc/uci-defaults/99-init-settings.sh
 if [[ "${RELEASE_BRANCH%:*}" == "openwrt" ]]; then
     echo "$RELEASE_BRANCH"
     sed -i '/# setup misc settings/ a\mv \/www\/luci-static\/resources\/view\/status\/include\/29_temp.js \/www\/luci-static\/resources\/view\/status\/include\/17_temp.js' files/etc/uci-defaults/99-init-settings.sh
