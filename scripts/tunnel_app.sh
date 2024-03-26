254e1b72686f2d317c60e92bba2c227ed3498776
friWrt <110967316+frizkyiman@users.noreply.github.com>
2024-03-26 11:40:12 +0700

    fix logic and tunnel option

--- scripts/tunnel_app.sh
+++ scripts/tunnel_app.sh
@@ -5,6 +5,11 @@ openclash_api="https://api.github.com/repos/vernesong/OpenClash/releases"
 openclash_file="luci-app-openclash"
 openclash_file_down="$(curl -s ${openclash_api} | grep "browser_download_url" | grep -oE "https.*${openclash_file}.*.ipk" | head -n 1)"
 
+# neko
+neko_api="https://api.github.com/repos/nosignals/neko/releases"
+neko_file="luci-app-neko"
+neko_file_down="$(curl -s https://api.github.com/repos/nosignals/neko/releases/latest | jq -r '.assets[] | select(.name | endswith("_23_05.ipk")) | .browser_download_url')"
+
 # passwall
 passwall_api="https://api.github.com/repos/xiaorouji/openwrt-passwall2/releases"
 passwall_file="passwall2_packages_ipk_$ARCH_3.zip"
@@ -23,11 +28,7 @@ passwall_ipk_packages=("https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/
                        "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/trojan-plus_10.0.3-2_$ARCH_3.ipk"
                        "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/ipt2socks_1.1.3-3_$ARCH_3.ipk"
                        "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/pdnsd-alt_1.2.9b-par-3_$ARCH_3.ipk")
-          
-# neko
-neko_api="https://api.github.com/repos/nosignals/neko/releases"
-neko_file="luci-app-neko"
-neko_file_down="$(curl -s https://api.github.com/repos/nosignals/neko/releases/latest | jq -r '.assets[] | select(.name | endswith("_23_05.ipk")) | .browser_download_url')"
+                       
 
 if [ "$1" == "openclash" ]; then
     echo "Downloading Openclash packages"
@@ -43,8 +44,28 @@ elif [ "$1" == "passwall" ]; then
     wget "${passwall2_file_down}" -nv -P packages
     unzip -qq packages/"$passwall_file" -d packages && rm packages/"$passwall_file"
     rm files/usr/bin/patchoc.sh
-elif [ "$1" == "openclash-passwall-neko" ]; then
+elif [ "$1" == "openclash-passwall" ]; then
     echo "Installing Openclash and Passwall"
+    echo "Downloading Openclash packages"
+    wget ${openclash_file_down} -nv -P packages
+    echo "Downloading Passwall packages ipk"
+    wget "$passwall_file_down" -nv -P packages
+    wget "$passwall_ipk" -nv -P packages
+    wget "${passwall_ipk_packages[@]}" -nv -P packages
+    wget "${passwall2_file_down}" -nv -P packages
+    unzip -qq packages/"$passwall_file" -d packages && rm packages/"$passwall_file"
+elif [ "$1" == "neko-passwall" ]; then
+    echo "Installing Neko and Passwall"
+    echo "Downloading Neko packages"
+    wget "${neko_file_down}" -nv -P packages
+    echo "Downloading Passwall packages ipk"
+    wget "$passwall_file_down" -nv -P packages
+    wget "$passwall_ipk" -nv -P packages
+    wget "${passwall_ipk_packages[@]}" -nv -P packages
+    wget "${passwall2_file_down}" -nv -P packages
+    unzip -qq packages/"$passwall_file" -d packages && rm packages/"$passwall_file"
+elif [ "$1" == "openclash-passwall-neko" ]; then
+    echo "Installing Openclash, Neko and Passwall"
     echo "Downloading Openclash packages"
     wget ${openclash_file_down} -nv -P packages
     echo "Downloading Neko packages"
