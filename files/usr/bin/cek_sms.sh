d6af59e16dc360c8ee57d5f55fccff60203774ee
friWrt <110967316+frizkyiman@users.noreply.github.com>
2024-04-21 15:07:15 +0700

    Update cek_sms.sh

--- files/usr/bin/cek_sms.sh
+++ files/usr/bin/cek_sms.sh
@@ -9,6 +9,7 @@ sms_ids=$(echo "$sms_list" | awk -F'/SMS/' 'NF>1{print $2}' | awk '{print $1}')
 
 if [ -n "$sms_ids" ]; then
     echo "SMS received from modem: $modem_info"
+    mmcli -m "$modem_id" --messaging-status
     for sms_id in $sms_ids; do
         echo "------------------------------------------"
         mmcli -m "$modem_id" --sms "$sms_id"
