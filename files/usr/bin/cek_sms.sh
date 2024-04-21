559149c6626a0a6fed3504956f3c27debe5ecd5b
friWrt <110967316+frizkyiman@users.noreply.github.com>
2024-04-21 14:48:04 +0700

    Create cek_sms.sh

---
+++ files/usr/bin/cek_sms.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Created by Frizkyiman
+# Github https://github.com/frizkyiman/
+
+modem_info=$(mmcli -L | awk -F']' '/Modem/{print $2}' | sed 's/^[ \t]*//')
+modem_id=$(mmcli -L | awk -F'/Modem/' 'NF>1{print $2; exit}' | awk '{print $1}')
+sms_list=$(mmcli -m "$modem_id" --messaging-list-sms)
+sms_ids=$(echo "$sms_list" | awk -F'/SMS/' 'NF>1{print $2}' | awk '{print $1}')
+
+if [ -n "$sms_ids" ]; then
+    echo "SMS received from modem: $modem_info"
+    for sms_id in $sms_ids; do
+        echo "------------------------------------------"
+        mmcli -m "$modem_id" --sms "$sms_id"
+        echo "------------------------------------------"
+    done
+else
+    echo "No sms messages were found from modem: $modem_info"
+fi
