0befaf717413985db3b524a0f3be9a7def5a4b12
friWrt <110967316+frizkyiman@users.noreply.github.com>
2024-04-29 21:42:31 +0700

    Update external-package-urls.sh

--- scripts/external-package-urls.sh
+++ scripts/external-package-urls.sh
@@ -16,7 +16,6 @@ files1=(
     "modemmanager|https://downloads.openwrt.org/releases/packages-23.05/$ARCH_3/packages"
     "libmbim|https://downloads.openwrt.org/releases/packages-23.05/$ARCH_3/packages"
     "libqmi|https://downloads.openwrt.org/releases/packages-23.05/$ARCH_3/packages"
-    "modeminfo|https://openwrt.132lan.ru/packages/$(echo "$BRANCH" | cut -d'.' -f1-2)/packages/$ARCH_3/modemfeed"
     "xmm-modem|https://openwrt.132lan.ru/packages/$(echo "$BRANCH" | cut -d'.' -f1-2)/packages/$ARCH_3/modemfeed"
     #"sms-tool|https://downloads.openwrt.org/releases/packages-23.05/$ARCH_3/packages"
     "luci-app-argon-config|https://fantastic-packages.github.io/packages/releases/$(echo "$BRANCH" | cut -d'.' -f1-2)/packages/$ARCH_3/luci"
